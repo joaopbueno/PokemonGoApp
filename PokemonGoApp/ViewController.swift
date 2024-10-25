3cfe04c1a7075f7f9360c2d78fc25337e89468da
joaopbueno <mrjoao16@gmail.com>
2024-10-25 10:39:02 -0300

    Configuração de mapa (Atualização)

--- PokemonGoApp/ViewController.swift
+++ PokemonGoApp/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    private lazy var mapview: MKMapView = {
+        let mapview = MKMapView()
+        mapview.translatesAutoresizingMaskIntoConstraints = false
+        mapview.showsUserLocation = true
+        
+        return mapview
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
+    
+    private func setupViews() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(mapview)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            mapview.topAnchor.constraint(equalTo: view.topAnchor),
+            mapview.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapview.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
 
 }
 
