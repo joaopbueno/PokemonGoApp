43c3e5aa55b0f9d1009bc993668f1e537b38a6bd
joaopbueno <mrjoao16@gmail.com>
2024-10-25 10:27:50 -0300

    Initial Commit

---
+++ PokemonGoAppTests/PokemonGoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonGoAppTests.swift
+//  PokemonGoAppTests
+//
+//  Created by João Bueno on 25/10/24.
+//
+
+import Testing
+@testable import PokemonGoApp
+
+struct PokemonGoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
